4bb4fd3496a950406c72ff6bf7c46c839036291f
Raul Camilo Martin <rcmartinb@correo.udistrital.edu.co>
2021-01-15 12:51:52 -0500

    A more complete aproximation of an app

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import time
 from datetime import datetime
 import os
 import SessionState
+import base64
+import plotly.express as px
 
 def app():
-
 
     st.write("""
     # Aplicación de informe de ArcGIS Online BMCR
@@ -19,19 +20,21 @@ def app():
     )
     option = st.selectbox("Use", ['File','Login'])
     if option == 'File':
+        sep = st.selectbox("Separator", [',',';','\t'])
+        encod = st.text_input("Encoding",value='utf8')
         file_data = st.file_uploader('Select file in csv:',type=['csv'])
         if file_data:
-            data = pd.read_csv(file_data,sep=';',encoding='utf8')
+            data = pd.read_csv(file_data,sep=sep,encoding=encod)
             EDA(data)
     else:
         portal = st.text_input("Enter a portal link", "https://www.arcgis.com/")
         st.write("""
-        ## Admin acount is required""")
+        #### Admin acount is required""")
         user = st.text_input("Enter the user",'rmartin_esri_colombia')
         password = st.text_input("Enter the password", type="password")
 
 
-        session_state = SessionState.get(name="", button_sent=False)
+        session_state = SessionState.get(name="login", button_sent=False)
 
         button_login = st.button("Login")
 
@@ -39,17 +42,37 @@ def app():
             session_state.button_sent = True
 
         if session_state.button_sent:
-            st.write(session_state.name)
+            # st.write(session_state.name)
 
-            try:
-                gis = GIS(portal,user,password)
-                st.write("Authentication succesfull")
-                try:
-                    EDA(arcgis_use(gis))
-                except:
-                    st.write('noshe que pasa')
-            except:
-                st.write("Authentication failed")
+            gis = GIS(portal,user,password)
+            st.write("Authentication succesfull")
+
+            data = arcgis_use(gis)
+            # st.write(data)
+
+            # try:
+            if type(data) == pd.core.frame.DataFrame:
+                EDA(data)
+            else:
+                st.write('')
+            # except:
+            
+
+
+
+            # try:
+            #     gis = GIS(portal,user,password)
+            #     st.write("Authentication succesfull")
+            #     try:
+            #         data = arcgis_use(gis)
+            #         try:
+            #             EDA(data)
+            #         except:
+            #             st.write('eda dont work')
+            #     except:
+            #         st.write('noshe que pasa')
+            # except:
+            #     st.write("Authentication failed")
 
             
 
@@ -60,46 +83,95 @@ def arcgis_use(gis):
     #### Actual credits: 
      {credits}
     """)
-    st.write('a sacar el inventario')
-    df = getInventary(gis)
-    # st.write(df)
-    file_name = 'temp.csv'
-    df.to_csv(file_name,encoding='utf8',sep=';',index=False)
+
+    users = {a.username:a for a in gis.users.search()}
+    
+    selected_usernames = st.multiselect("Select users (default all)",list(users.keys()),list(users.keys()))
+    selected_users = [v for k,v in users.items() if k in selected_usernames]
+        
+    df = getInventary(selected_users)
+    st.markdown(get_table_download_link(df), unsafe_allow_html=True)
     return df
 
 
+
 def EDA(df):
-    st.write('cagado papa')
-    st.write("Total storage: {:.2f} Mb == {:.2f} Gb"\
-            .format(df['Size (mb)'].sum(),
-            df['Size (mb)'].sum()/1024))
+    st.write("""
+        #### Storage""")
+    try:
+        st.write("Total storage: {:.2f} Mb == {:.2f} Gb"\
+                .format(df['Size (mb)'].sum(),
+                df['Size (mb)'].sum()/1024))
+    except:
+        st.write('No Size (mb) columns founded')
 
-    # var = st.selectbox("Filter by", ['Variable']+list(df))
-    var = st.multiselect("Filter by",list(df))
-    # st.write(vars)
-    n_max = st.number_input('Registers to show',min_value=0,value=10,step=1)
-    sortby = st.selectbox("Sort by", list(df.select_dtypes(include=['float64','int64'])))
+        # var = st.selectbox("Filter by", ['Variable']+list(df))
+    st.write("""
+        #### General view""")
+    var = st.multiselect("Variable",list(df))
 
-    
-    if var == False:
+    n_max = st.number_input('Registers to show',min_value=0,max_value = len(df.index), value=10,step=1)
+    number_columns = list(df.select_dtypes(include=['float64','int64']))
+    sortby = st.selectbox("Sort by", number_columns)
+    not_sortby = [i for i in number_columns if i != sortby][0]
+    if var == []:
         st.dataframe(df.sort_values(by=sortby,ascending=False).head(n_max))
     else:
-        df_show = df.groupby(var)\
-                    .sum()\
-                    .reset_index()\
-                    .sort_values(by=sortby,ascending=False)\
-                    .head(n_max)
-        st.dataframe(df_show)
+        filename = '_'.join(var)
+        df_show = df.drop(columns=not_sortby)\
+                        .groupby(var)\
+                        .sum()\
+                        .reset_index()\
+                        .sort_values(by=sortby,ascending=False)
+        st.markdown(get_table_download_link(df_show,'this table',comp=f'_{filename}'), unsafe_allow_html=True)
+        st.dataframe(df_show.head(n_max))
+        # st.write(df_show_0.columns)
+        if len(var)==0:
+            fig = px.bar(df.head(n_max), x=var[0], y=sortby)
+            st.plotly_chart(fig)
+        else:
+            fig = px.bar(df.head(n_max), x=var[0], y=sortby)
+            st.plotly_chart(fig)
+    st.write("""
+        #### Filtered view""")
+    sortby_2 = st.selectbox("Sort by:", number_columns)
+    not_sortby_2 = [i for i in number_columns if i != sortby_2][0]
+    filter_by = st.selectbox("Filter by", list(df))
+    n_max_2 = st.number_input('Registers to show :',min_value=0, value=10,step=1)
 
+    if filter_by in number_columns:
+        value_to_filter = st.number_input('Higher than :',min_value=0, value=100,step=1)
+        df_filtered = df[df[filter_by] >= value_to_filter]\
+                        .sort_values(by=sortby_2,ascending=False)
+    else:
+        value_to_filter = st.selectbox(f"{filter_by} equals to ", df[filter_by].unique())
+        df_filtered = df[df[filter_by] == value_to_filter]\
+                        .sort_values(by=sortby_2,ascending=False)
+    
+    filename_2 = f'{filter_by}_{value_to_filter}'
+    st.markdown(get_table_download_link(df_filtered,'this table',comp=f'_{filename_2}'), unsafe_allow_html=True)
+    st.dataframe(df_filtered)
+        
+    
 
-def getInventary(gis):
+def get_table_download_link(df,name = 'all the table',comp=''):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="inventary{comp}.csv">Download {name} in csv file</a>'
+    return href
+
+def getInventary(users):
     columns = ["Folder","Owner", "Title", "Name", "Type", "Views", 
            "Size (mb)", "Creation date", "Edit date", "Item ID","Acces","Url"]
     df_i = pd.DataFrame()
     my_bar = st.progress(0)
 
     ##limit the users
-    for user in gis.users.search():
+    for user in users:
         datos = getInventaryItems(user)
         df = pd.DataFrame(datos, columns=columns)
         df_i = pd.concat([df_i,df])
@@ -119,7 +191,7 @@ def getInventaryItems(user):
                           item.title, 
                           item.name, 
                           item.type, 
-                          item.numViews, 
+                          int(item.numViews), 
                           item.size/1048576 , 
                           datetime.utcfromtimestamp(item.created/1000).strftime('%Y-%m-%d %H:%M:%S'),
                           datetime.utcfromtimestamp(item.modified/1000).strftime('%Y-%m-%d %H:%M:%S'),
